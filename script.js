251cefd87dd740e8dc488579c262d6c805eb3744
Md-Shahzadah-Sattar <148210694+Md-Shahzadah-Sattar@users.noreply.github.com>
2023-12-24 20:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-function makeid(l) {
-  // write your code here
+function makeid(length) {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
 }
 
+// Examples
+const randomString1 = makeid(5);
+const randomString2 = makeid(10);
+
+console.log(randomString1); // Example output: "K3f2G"
+console.log(randomString2); // Example output: "aB9EfgT2X1"
+
+
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+// const l = prompt("Enter a number.");
+// alert(makeid(l));
